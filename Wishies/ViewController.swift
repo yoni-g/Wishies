1846b489e6c40d395199e559609b03e796bb576c
Yoni Goriachnick <yonig770@gmail.com>
2020-03-20 14:44:39 +0200

    Initial Commit

---
+++ Wishies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wishies
+//
+//  Created by Yonathan Goriachnick on 20/03/2020.
+//  Copyright © 2020 YGApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
