2023615fcdb782b4d7c438ac39204fd6fb2e6363
Yoni Goriachnick <yonig770@gmail.com>
2020-04-13 20:19:46 +0300

    Added SettingsVC and VM

---
+++ Wishies/Screens/Settings/SettingsViewModal.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewModal.swift
+//  Wishies
+//
+//  Created by Yonathan Goriachnick on 13/04/2020.
+//  Copyright © 2020 YGApps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewModal{
+    
+}
